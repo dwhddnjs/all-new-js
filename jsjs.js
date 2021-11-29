8c80b447790c3a5d114fa346598674c3237dd368
dwhddnjs <per1215@naver.com>
2021-11-29 10:49:49 +0900

    practiced js

---
+++ jsjs.js
@@ -0,0 +1,27 @@
+출력방법;
+
+window.console.log("hello world");
+
+document.write("hello world");
+
+document.getElementById("one").innerHTML = "hi";
+
+window.alert("hello world");
+
+console.dir(window);
+
+window.console.log();
+
+console.error();
+
+console.table();
+
+let tableSample = {
+  name: "hojun",
+  age: 10,
+};
+
+let result = 200;
+console.log(result);
+let result = 100;
+console.log(result);
