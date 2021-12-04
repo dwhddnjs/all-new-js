1a2e1b1f784ddb4a6e42a35d5bcca24af1785983
dwhddnjs <per1215@naver.com>
2021-12-04 23:48:27 +0900

    added code

--- function.js
+++ function.js
@@ -1,14 +1,14 @@
-// let k = 10;
-// function viewk() {
-//   console.log(k);
-// }
-// function changek() {
-//   k += 10;
-//   console.log(k);
-// }
-// viewk();
-// changek();
-// viewk();
+let k = 10;
+function viewk() {
+  console.log(k);
+}
+function changek() {
+  k += 10;
+  console.log(k);
+}
+viewk();
+changek();
+viewk();
 
 let k = 10;
 function viewk() {
@@ -35,21 +35,18 @@ add();
 add((b = 300));
 500;
 
-
 //함수
 //함수의 입력 출력 기능을 하나로 묶어 재사용 할 수 있도로 만드는 것
 //자바스크립트의 실행코드들을 들어 있는 독립 블록 단위의 객체인 함수를 사용
 
 // 기본적 구조 : function(){}
 
-
-
 function myFunction(x, y) {
   z = x + y;
   return z;
 }
 
-myFunction(10, 20)
+myFunction(10, 20);
 
 //function: 함수 선언 키워드
 //myFunction: 함수명
