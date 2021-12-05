0c4c2135e471a41441ed7864d93253a7ff89bb79
dwhddnjs <per1215@naver.com>
2021-12-05 22:55:40 +0900

    remind func

--- random.js
+++ random.js
@@ -1,3 +1,26 @@
+// function randomNum(min, max) {
+//   return Math.floor(Math.random() * (max - min) + min);
+// }
+
+// let lottoArr = [];
+// function lottoGenerator() {
+//   while (lottoArr.length < 6) {
+//     let result = randomNum(1, 46);
+//     let isGot = false;
+
+//     // 중복 판단
+//     lottoArr.forEach((el) => {
+//       if (el === result) isGot = true;
+//     });
+
+//     // 중복이 없다면 배열에 추가
+//     if (!isGot) lottoArr.push(result);
+//   }
+//   return lottoArr;
+// }
+
+// console.log(lottoGenerator());
+
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
@@ -8,15 +31,11 @@ function lottoGenerator() {
     let result = randomNum(1, 46);
     let isGot = false;
 
-    // 중복 판단
     lottoArr.forEach((el) => {
       if (el === result) isGot = true;
     });
-
-    // 중복이 없다면 배열에 추가
     if (!isGot) lottoArr.push(result);
   }
   return lottoArr;
 }
-
 console.log(lottoGenerator());
