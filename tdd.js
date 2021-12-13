315a9cab83414a2a785ff13b0e5b2ab67631b40f
dwhddnjs <per1215@naver.com>
2021-12-13 15:56:41 +0900

    add todd

---
+++ tdd.js
@@ -0,0 +1,17 @@
+describe("클릭이벤처리 및 뷰를 담당하는 함수입니다.", () => {
+  //dependency injection (의존 주입)
+  it("viewManager 에 인자가 잘 전달됐는지 확인합니다.", () => {
+    const textManager = null;
+    const btnEl = null;
+    const viewerEl = null;
+    const inpTxt = null;
+
+    // const viewManager = new ViewManager(textManager, { btnEl, viewerEl, inpTxt });
+
+    // 인자가 전달되는지 확인하는 함수
+    const actual = () =>
+      new ViewManager(textManager, { btnEl, viewerEl, inpTxt });
+
+    expect(actual).toThrowError();
+  });
+});
